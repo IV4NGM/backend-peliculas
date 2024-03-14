79d74cf75c37db85ac5575f1bff51f253235880f
IvanGM <ivangm_01@hotmail.com>
2024-03-14 02:50:12 -0600

    style: Errores mostrados en consola

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 400 : res.statusCode
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(err.stack)
+  }
   res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack
